8135d7a5b5730ae4455a4da5df897a34139c4364
王佳 <wangjia@cvte.cn>
2015-04-01 14:52:00 +0800

    增加了对外使用的接口

--- app/src/main/java/com/jacob/circle/disc/CircleIndexView.java
+++ app/src/main/java/com/jacob/circle/disc/CircleIndexView.java
@@ -10,6 +10,7 @@ import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * Package : com.jacob.circle.disc
@@ -19,9 +20,26 @@ import android.view.ViewGroup;
  */
 public class CircleIndexView extends ViewGroup {
 
+    private Context mContext;
     private Bitmap mBitmapBg;
 
     private boolean isShowCircle = false;
+
+    private static float sPerAngle = 360.0f / 26;
+
+    private String[] mLetters = new String[]{
+            "A", "B", "C",
+            "D", "E", "F",
+            "G", "H", "I",
+            "J", "K", "L",
+            "M", "N", "O",
+            "P", "Q", "R",
+            "S", "T", "U",
+            "V", "W", "X",
+            "Y", "Z"};
+
+    private String mCurrentLetters;
+    private OnIndexChangeListener mIndexListener;
 
     public CircleIndexView(Context context) {
         this(context, null);
@@ -33,6 +51,7 @@ public class CircleIndexView extends ViewGroup {
 
     public CircleIndexView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mContext = context;
         mBitmapBg = BitmapFactory.decodeResource(getResources(), R.mipmap.roulette);
     }
 
@@ -75,17 +94,14 @@ public class CircleIndexView extends ViewGroup {
     @Override
     protected void onDraw(Canvas canvas) {
 //        super.onDraw(canvas);
-//        canvas.drawColor(Color.GRAY);
         if (isShowCircle) {
             canvas.drawBitmap(mBitmapBg, 0, 0, null);
         }
-
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (changed) {
-            int count = getChildCount();
             View view = getChildAt(0);
             int childW = view.getMeasuredWidth();
             int childH = view.getMeasuredHeight();
@@ -96,15 +112,25 @@ public class CircleIndexView extends ViewGroup {
         }
     }
 
+    private float mLastX;
+    private float mLastY;
+
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        float x = 0;
+        float y = 0;
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 isShowCircle = true;
+                x = event.getX();
+                y = event.getY();
+
                 break;
             case MotionEvent.ACTION_MOVE:
                 isShowCircle = true;
+
+
                 break;
             case MotionEvent.ACTION_CANCEL:
             case MotionEvent.ACTION_UP:
@@ -115,7 +141,47 @@ public class CircleIndexView extends ViewGroup {
         return true;
     }
 
+
+    /**
+     * 获取当前字母的序号
+     */
+    private int getLetterPosition(String letter) {
+        int length = mLetters.length;
+        for (int i = 0; i < length; i++) {
+            if (letter.toUpperCase().equals(mLetters[i])) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+    public void setIndexLetter(String letter) {
+        if (letter == null) return;
+        if (letter.length() > 1) {
+            letter = letter.substring(0, 1);
+        }
+        this.mCurrentLetters = letter;
+        updateChildDrawable(mCurrentLetters);
+    }
+
+
+    private void updateChildDrawable(String letter) {
+        int drawable = getResources().getIdentifier("letter_" + letter, "mipmap", mContext.getPackageName());
+        ImageView view = (ImageView) getChildAt(0);
+        view.setImageResource(drawable);
+    }
+
     private int dpToPx(int dp) {
         return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
+    }
+
+
+    public void setOnIndexChangeListener(OnIndexChangeListener listener) {
+        this.mIndexListener = listener;
+    }
+
+    public interface OnIndexChangeListener {
+        void onIndexChange(String letter);
     }
 }
--- app/src/main/java/com/jacob/circle/disc/MainActivity.java
+++ app/src/main/java/com/jacob/circle/disc/MainActivity.java
@@ -2,14 +2,30 @@ package com.jacob.circle.disc;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.widget.TextView;
 
 
 public class MainActivity extends FragmentActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.textView);
+
+        CircleIndexView circleIndexView = (CircleIndexView) findViewById(R.id.circle_index_view);
+        circleIndexView.setOnIndexChangeListener(new CircleIndexView.OnIndexChangeListener() {
+            @Override
+            public void onIndexChange(String letter) {
+                mTextView.setText(letter);
+            }
+        });
+        circleIndexView.setIndexLetter("b");
+
+
     }
 
 }
